5210f6cb0a9f8a318c2dd550044efc1878b39607
Pinguu-dev <72171280+Pinguu-dev@users.noreply.github.com>
2022-02-23 00:12:52 +0100

    4.03

--- valorantoffsets.hpp
+++ valorantoffsets.hpp
@@ -1,7 +1,7 @@
 namespace Offsets {
 	// global
-	uint64_t uworld_key = 0x8FB6978;
-	uint64_t uworld_state = 0x8FB6940;
+	uint64_t uworld_key = 0x8F66E38; 
+	uint64_t uworld_state = 0x8F66E00;
 	uint64_t persistent_level = 0x38;			// world > persistent_level
 	uint64_t game_instance = 0x1A8;				// world > game_instance
  
@@ -9,14 +9,14 @@ namespace Offsets {
 	uint64_t localplayers_array = 0x40;			// world > game_instance > localplayers_array
 	uint64_t localplayer = 0x40;				// world > game_instance > localplayers_array[0]
 	uint64_t player_controller = 0x38;			// world > game_instance > localplayers_array[0] > playercontroller
-	uint64_t apawn = 0x440;					// world > game_instance > localplayers_array[0] > playercontroller > apawn									// aactor > apawn
+	uint64_t apawn = 0x460;					// world > game_instance > localplayers_array[0] > playercontroller > apawn									// aactor > apawn
  
 	// vector
 	uint64_t root_component = 0x250;			// world > game_instance > localplayers_array[0] > playercontroller > apawn > root_component				// aactor > root_component
 	uint64_t position = 0x164;				// world > game_instance > localplayers_array[0] > playercontroller > apawn > root_component > position		// aactor > root_component > position
 	
 	// controllers
-	uint64_t damage_controller = 0x998;			// world > game_instance > localplayers_array[0] > playercontroller > apawn > damage_controller				// aactor > damage_controller
+	uint64_t damage_controller = 0x9A0;			// world > game_instance > localplayers_array[0] > playercontroller > apawn > damage_controller				// aactor > damage_controller
 	uint64_t camera_controller = 0x420;			// world > game_instance > localplayers_array[0] > playercontroller > camera_controller
  
 	// camera
@@ -34,7 +34,7 @@ namespace Offsets {
 	uint64_t team_component = 0x5A0;			// world > persistent_level > aactor > player_state > team_component
 	uint64_t team_id = 0xF8;				// world > persistent_level > aactor > team_component > team_id
 	uint64_t health = 0x1B0;				// world > persistent_level > aactor > damage_controller > health
-	uint64_t dormant = 0x100;				// world > persistent_level > aactor > dormant
+	uint64_t dormant = 0x120;				// world > persistent_level > aactor > dormant
 	uint64_t player_state = 0x3F0;				// world > persistent_level > aactor > player_state
  
 	// mesh
