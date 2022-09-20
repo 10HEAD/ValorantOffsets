94fddd66c82d77aa2d07d0143d91f7e656dbd56c
Pinguu-dev <72171280+Pinguu-dev@users.noreply.github.com>
2022-09-20 19:21:01 +0200

    05.06.00.765767

--- valorantoffsets.hpp
+++ valorantoffsets.hpp
@@ -1,13 +1,13 @@
 namespace offsets {
 	// global
-	uint64_t uworld_key = 0x8F24238; 
-	uint64_t uworld_state = 0x8F24200;
-	uint64_t g_object_state = 0x8D92580;
-	uint64_t g_object_key = 0x8D925B8;
-	uint64_t line_of_sight = 0x4977040;
-	uint64_t bone_matrix = 0x493D160;
+	uint64_t uworld_key = 0x90217F8; 
+	uint64_t uworld_state = 0x90217C0;
+	uint64_t g_object_state = 0x8E8FB00;
+	uint64_t g_object_key = 0x8E8FB38;
+	uint64_t line_of_sight = 0x2A0C680;
+	uint64_t bone_matrix = 0x49F14C0;
 	
-	uint64_t pakman_offset = 0x1B84120;                     // deref_pointer_in_game_space_fn - https://www.unknowncheats.me/forum/valorant/503616-dumping-valorant-perfect-results-easy.html
+	uint64_t pakman_offset = 0x1C1C4B0;                     // deref_pointer_in_game_space_fn - https://www.unknowncheats.me/forum/valorant/503616-dumping-valorant-perfect-results-easy.html
 	
 	// world
 	uint64_t persistent_level = 0x38;			// world > persistent_level
