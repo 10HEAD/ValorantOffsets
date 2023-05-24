3f7207da2a157c34d155c120f222ca23ec38ee56
10HEAD <68811142+10HEAD@users.noreply.github.com>
2023-05-24 16:19:10 +0200

    06.10.00.885592

--- valorantoffsets.hpp
+++ valorantoffsets.hpp
@@ -1,43 +1,44 @@
 namespace offsets {
-	const _qword uworld_state = 0x8c8a6c0;
+	const _qword uworld_state = 0x9282500;
 	const _qword uworld_key = uworld_state + 0x38;
-	
+ 
 	const _qword gobject_state = null;
 	const _qword gobject_key = gobject_state + 0x38;
-	
-	const _qword gdynamic_rhi = 0x8c13e88;
-	const _qword get_view_point = 0x4cf72b0;
-	
-	const _qword fnamepool = 0x8ac0e00;
-	const _qword fnamepool_state = 0x8af3dc0;
+ 
+	const _qword gdynamic_rhi = 0x9209288;
+	const _qword get_view_point = 0x5195710;
+ 
+	const _qword fnamepool = 0x9083980;
+	const _qword fnamepool_state = 0x90b6940;
 	const _qword fnamepool_key = fnamepool_state + 0x38;
-	const _qword fnamepool_decrypt_wide = 0x313ddd0;
-	const _qword fnamepool_decrypt_non_wide = 0x313dd60;
-	
-	const _qword static_load_object = 0x33b1f30;
-	const _qword static_find_object = 0x33b1480;
-	const _qword bypass_skin_changer = 0x23db3e0;
-	const _qword line_of_sight = 0x4aa8960;
-	const _qword bone_matrix = 0x4a79c90;
-	const _qword project_world_to_screen = 0x4bc72e0;
-	const _qword get_actor_bounds = 0x4856140;
-	const _qword process_event = 0x339cec0;
-	const _qword trigger_veh = 0x1aa9d80;
-	const _qword free_memory = 0x3038c90;
-	
-	const _qword get_firing_location = 0x28a26a0;
-	const _qword get_spread_angles = 0x2a62320;
-	const _qword get_spread_values = 0x23a63a0;
-	const _qword to_vector_and_normalize = 0x30c0510;
-	const _qword to_angle_and_normalize = 0x30ba270;
-	
-	const _qword error_power = 0x3f8;
-	const _qword error_retries = 0x3fc:
-	const _qword firing_state = 0x1020;
-	const _qword networked_random_number_generator_component = 0x428;
-	const _qword registered_stability_component = 0x418;
-	
-	const _qword k2_draw_line = 0x50f9e00;
-	const _qword k2_draw_text = 0x50fa580;
-	const _qword k2_draw_box = 0x50f9d40;
+	const _qword fnamepool_decrypt_wide = 0x329a020;
+	const _qword fnamepool_decrypt_non_wide = 0x3299fb0;
+ 
+	const _qword static_load_object = 0x3520070;
+	const _qword static_find_object = 0x351f5c0;
+	const _qword bypass_skin_changer = 0x2acd850;
+	const _qword ares_outline_mode = 0x4ebdc60;
+	const _qword line_of_sight = 0x4f3d090;
+	const _qword bone_matrix = 0x4f0ed20;
+	const _qword project_world_to_screen = 0x505d120;
+	const _qword get_actor_bounds = 0x4ce2cd0;
+	const _qword process_event = 0x350ac50;
+	const _qword trigger_veh = 0x1b6a4e0;
+	const _qword free_memory = 0x318d3d0;
+ 
+	const _qword get_firing_location = 0x29e0380;
+	const _qword get_spread_angles = 0x2ba3ec0;
+	const _qword get_spread_values = 0x25039b0;
+	const _qword to_vector_and_normalize = 0x321a430;
+	const _qword to_angle_and_normalize = 0x3216c20;
+ 
+	const _qword error_power = 0x450;
+	const _qword error_retries = 0x454:
+	const _qword firing_state = 0x42c;
+	const _qword networked_random_number_generator_component = 0x480;
+	const _qword registered_stability_component = 0x470;
+
+	const _qword k2_draw_line = 0x55b5fc0;
+	const _qword k2_draw_text = 0x55b6750;
+	const _qword k2_draw_box = 0x55b5f00;
 }
