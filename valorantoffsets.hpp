253ac670252f5c3bf9d47053ed5fdf14ae8c5375
10HEAD <68811142+10HEAD@users.noreply.github.com>
2023-07-27 13:29:02 +0200

    07.01.00.932809

--- valorantoffsets.hpp
+++ valorantoffsets.hpp
@@ -1,44 +1,46 @@
-namespace offsets {
-	const _qword uworld_state = 0x92df200;
-	const _qword uworld_key = uworld_state + 0x38;
- 
-	const _qword gobject_state = null;
-	const _qword gobject_key = gobject_state + 0x38;
- 
-	const _qword gdynamic_rhi = 0x9265f38;
-	const _qword get_view_point = 0x51d3380;
- 
-	const _qword fnamepool = 0x90e0600;
-	const _qword fnamepool_state = 0x91135c0;
-	const _qword fnamepool_key = fnamepool_state + 0x38;
-	const _qword fnamepool_decrypt_wide = 0x32d79f0;
-	const _qword fnamepool_decrypt_non_wide = 0x32d7980;
- 
-	const _qword static_load_object = 0x355d8b0;
-	const _qword static_find_object = 0x355ce00;
-	const _qword bypass_skin_changer = 0x252e700;
-	const _qword ares_outline_mode = 0x4efb760;
-	const _qword line_of_sight = 0x4f7ab90;
-	const _qword bone_matrix = 0x4f4c840;
-	const _qword project_world_to_screen = 0x509ace0;
-	const _qword get_actor_bounds = 0x4d20770;
-	const _qword process_event = 0x3548490;
-	const _qword trigger_veh = 0x1b81b10;
-	const _qword free_memory = 0x31cada0;
- 
-	const _qword get_firing_location = 0x2a0d000;
-	const _qword get_spread_angles = 0x2bd8000;
-	const _qword get_spread_values = 0x24f8940;
-	const _qword to_vector_and_normalize = 0x3257e00;
-	const _qword to_angle_and_normalize = 0x32545f0;
-	
-	const _qword error_power = 0x450;
-	const _qword error_retries = 0x454:
-	const _qword firing_state = 0x1020;
-	const _qword networked_random_number_generator_component = 0x480;
-	const _qword registered_stability_component = 0x470;
-	
-	const _qword k2_draw_line = 0x55f38c0;
-	const _qword k2_draw_text = 0x55f4050;
-	const _qword k2_draw_box = 0x55f3800;
-}
+	namespace offsets {
+        //cc hook
+		const _qword static_find_object = 0x3bba680;
+		const _qword call_remote_function = 0x3ba5fe0; //pe
+
+		const _qword bone_matrix = 0x558d060;
+		const _qword bypass_skin_changer = 0x2b61c10;
+
+		const _qword set_ares_outline = 0x553c190;
+
+       		 //dx render
+        	const _qword game_pointer = NULL;
+        	const _qword game_instance = 0x1a0;
+
+        	const _qword u_level = 0x38;
+        	const _qword local_players = 0x40;
+        	const _qword player_controller = 0x38;
+        	const _qword player_state = 0x3f0;
+
+        	const _qword player_camera_manager = 0x478;
+        	const _qword my_hud = 0x470;
+
+        	const _qword acknowledged_pawn = 0x460;
+
+        	const _qword team_component = 0x628;
+        	const _qword team_id = 0xf8;
+
+        	const _qword unique_id = 0x38;
+        	const _qword fname_id = 0x18;
+
+        	const _qword aactor_array = 0xa0;
+
+        	const _qword root_component = 0x230;
+        	const _qword relative_location = 0x164;
+
+        	const _qword mesh_component = 0x430;
+        	const _qword damage_handler = 0x9f8;
+
+        	const _qword is_dormant = 0x100;
+        	const _qword health = 0x1b0;
+        	const _qword component_to_world = 0x250;
+
+        	const _qword bone_array = 0x5c8;
+        	const _qword bone_array_cache = bone_array + 0x10;
+        	const _qword bone_count = bone_array + 0x8;
+	}
