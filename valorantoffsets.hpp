deff8daec5f1f9fad978d601a311aabc18ce796b
Linus <68811142+10HEAD@users.noreply.github.com>
2025-03-25 00:22:12 +0100

    10.05.00.3367018

--- valorantoffsets.hpp
+++ valorantoffsets.hpp
@@ -1,15 +1,15 @@
 	namespace offsets {
-		const _qword static_find_object = 0x49cc110;
+		const _qword static_find_object = 0x4996570;
 		const _qword static_load_object = static_find_object + 0xab0;
 
-		const _qword call_remote_function = 0x49B7ac0;
+		const _qword call_remote_function = 0x4981f20;
 
-		const _qword bone_matrix = 0x648dfc0;
+		const _qword bone_matrix = 0x6458700;
 		
-		const _qword bypass_skin_changer = 0x33c3330;
+		const _qword bypass_skin_changer = 0x3394710;
 		const _qword skin_changer_1 = 0x360;
 		const _qword skin_changer_2 = 0xb0;
 		const _qword skin_changer_3 = 0x80;
 
-		const _qword set_ares_outline = 0x643ccf0;
+		const _qword set_ares_outline = 0x6407430;
 	}
